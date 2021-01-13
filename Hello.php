ff72006240b3bc8c4d329bef39ceb9f70ac897e9
sptakyw <sptakyw@gmail.com>
2021-01-13 10:05:33 +0700

    create Hello.php

---
+++ Hello.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Hello </title>
+    </head>
+    <body>
+    <?php
+            echo ("<h> Hello </h1> <br>") ;
+            echo ("<h2> Prachinburi <h2>") ;
+        ?>
+    </body>
+</html> 
