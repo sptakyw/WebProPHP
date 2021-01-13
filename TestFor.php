6c14c22435af07404c67822b23ff89147505836a
sptakyw <sptakyw@gmail.com>
2021-01-13 10:41:11 +0700

    create TestFor.php

---
+++ TestFor.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+    <center>
+        <?php
+            for($i = 1 ; $i <= 10 ; $i++)
+            {
+                echo ( "<font size=\"".$i."\"> Welcome to PHP world </font><br/>" ) ;   
+            }
+        ?>
+    </center>
+    </body>
+</html> 
