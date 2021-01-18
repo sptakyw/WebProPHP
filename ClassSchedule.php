09b6278c5371ebea2782487fd9183f2af980632d
sptakyw <sptakyw@gmail.com>
2021-01-18 20:40:26 +0700

    create ClassSchedule.php

---
+++ ClassSchedule.php
@@ -0,0 +1,96 @@
+<html lang = "en">
+<head>
+<meta charset = "UTF-8">
+<title> Class schedule </title>
+
+ <body>
+
+    <table border = "1" align = "center" width = "1500">
+        <tr>
+            <td colspan = "14" align = "center"> ♡ IT2RA ♡  </td>
+        <tr>
+
+        <tr>
+            <td align = "center"> ♡ Day ♡ </td>
+            <td align = "center"> 08:00 </td>
+            <td align = "center"> 09:00 </td>
+            <td align = "center"> 10:00 </td>
+            <td align = "center"> 11:00 </td>
+            <td align = "center"> 12:00 </td>
+            <td align = "center"> 13:00 </td>
+            <td align = "center"> 14:00 </td>
+            <td align = "center"> 15:00 </td>
+            <td align = "center"> 16:00 </td>
+            <td align = "center"> 17:00 </td>
+            <td align = "center"> 18:00 </td>
+            <td align = "center"> 19:00</td>
+            <td align = "center"> 20:00  </td> 
+        </tr> 
+        
+        <tr>
+            <td align = "center"> ♡ Monday ♡ </td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+            <td align = "center"> ♡ Lunch break ♡</td>
+            <td align = "center"> 060243111 </td>
+            <td align = "center"> Software </td>
+            <td align = "center"> Enginerring </td>
+            <td align = "center">  </td>
+
+            <td align = "center"> 061100001 </td>
+            <td align = "center"> Design </td>
+            <td align = "center"> Thinking </td>
+           
+        </tr>   
+
+        <tr>
+            <td align = "center"> ♡ Tuesday ♡ </td>
+            <td align = "center">  </td>
+            <td align = "center"> 080103034 </td>
+            <td align = "center"> English </td>
+            <td align = "center"> Conversation </td>
+            <td align = "center"> ♡ Lunch break ♡</td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+
+            <td align = "center"> 060243112  </td>
+            <td align = "center"> System Analysis </td>
+            <td align = "center"> And Design </td>
+        </tr>
+
+        <tr>
+            <td align = "center"> ♡ Wednesday ♡ </td>
+            <td align = "center"> 060243113 </td>
+            <td align = "center"> Web S.1 </td>
+            <td align = "center"> Programming </td>
+            <td align = "center"> L.1  </td>
+            <td align = "center"> ♡ Lunch break ♡</td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+            <td align = "center">  </td>
+
+            <td align = "center"> 06024110 </td>
+            <td align = "center"> JavaScript S.1 </td>
+            <td align = "center"> Programming </td>
+            <td align = "center"> L.1  </td>
+        </tr>
+
+        <tr>
+        <td align = "center"> ♡ Thursday ♡ </td>
+        </tr>
+
+        <tr>
+        <td align = "center"> ♡ Friday ♡ </td>
+        </tr>
+
+            <?php
+                echo "<b> ● CLASS SCHEDULE IT2RA ♡ </b>" ;
+            ?>
+
+</head>
+    </body>
+</html>
