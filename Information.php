e03bd0f56b56b2831931b4e9cf9e1324799784ff
sptakyw <sptakyw@gmail.com>
2021-01-19 17:48:13 +0700

    create Information.php

---
+++ Information.php
@@ -0,0 +1,114 @@
+<hthml>
+<head>
+<title> Information </title>
+</head>
+<body bgcolor = "#CC99CC">
+
+    <table border = "0" align = "center" width = "600" bgcolor = "#FFFF66">
+        <tr>
+            <td colspan = "1" align = "left">   
+
+            <form>
+        title
+            <select>
+                <option> Mr.
+                <option> Mrs.
+                <option> Miss
+            </select>
+        </form>
+            
+            </td>
+            <td colspan = "1" align = "center" > name - surname  </td>
+            <td align = "center"> <input type = "text" name = "s" size = "20" value = "" /> </td>
+            <td align = "center"> <input type = "text" name = "s" size = "20" value = "" /> </td>
+            <tr>
+    
+        <tr>  
+            <td>    
+        <form>
+        age
+            <select>
+                <option> 18 - 25
+                <option> 26 - 35
+                <option> 36 - 45
+                <option> 46 - 55
+                <option> 56 - 65
+                <option> 66 - 70+       
+            </select>
+        </form>
+        </td>
+
+        <td>
+        <form>
+        sex
+            <select>
+                <option> ชาย
+                <option> หญิง      
+            </select>
+        </form>
+        
+        <td colspan = "1" align = "center" > nickname </td>
+        <td align = "center"> <input type = "text" name = "s" size = "20" value = "" /> </td>
+
+        </td>
+        </tr>
+
+        <tr>
+        <td>
+        <form>
+        day ;
+            <select>
+                <option> monday
+                <option> tuesday
+                <option> wednesday
+                <option> thursday
+                <option> friday  
+                <option> saturday
+                <option> sunday
+            </select>
+        </td>
+
+        <td>
+        <form>
+        month ;
+            <select>
+                <option> january
+                <option> february
+                <option> march
+                <option> april
+                <option> may        
+                <option> june
+                <option> july
+                <option> august
+                <option> september
+                <option> october
+                <option> november
+                <option> december
+            </select>
+        </form>
+        </td>
+
+        <td>
+        <form>
+        year ;
+            <select>
+                <option> 1950-1990
+                <option> 1991-2000
+                <option> 2001-2010
+                <option> 2010-2021
+            </select>
+        </form>
+
+        <tr>
+        <td> <input type = "submit"> </td>
+        </tr>
+
+        </td> 
+        </tr>            
+        
+    <?php
+    echo "✿ 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝙏𝙄𝙊𝙉 • ﻌ • " ;
+    ?>
+
+</body>
+</html>
