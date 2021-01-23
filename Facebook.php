de71258219e583323d99f9401016818fb1e39762
sptakyw <sptakyw@gmail.com>
2021-01-23 23:21:32 +0700

    create Facebook.php

---
+++ Facebook.php
@@ -0,0 +1,141 @@
+<html>
+<head>
+<title> Facebook </title>
+<body bgcolor = "#e2e5ef">
+    
+    <table width = 100% style = "border:15px dashed #3b5999;" cellspacing = "5" bgcolor = "#3b5999" cellpadding = "2">
+    <tr>
+        <td> <font Face = "DS-Silom" size = "10" color = "white" > facebook </font> 
+        </td> 
+   
+            <td width = 90% colspan = "1" align = "right"> 
+            <font Face = "Calibri Light" size = "3" color = "white" > Email or Phone </td>
+            <td width = 100% colspan = "1" align = "left">
+            <font Face = "Calibri Light" size = "3" color = "white" > Password </td>
+            <tr>
+           
+            
+        <tr>
+        <td align = "center">  </td>
+            <td align = "right"> <input type = "text" name = "email" size = "20" value = "" /> </td>
+            <td align = "right"> <input type = "password" name = "password" size = "20" value = "" /> </td>
+        
+        
+            <td colspan = "3" align = "center">
+                <input type = "submit" value = "Log in" />
+            <td>
+        <tr>
+ </tr>
+    </table>
+
+    <table border = "0" align = "left" width = "500">
+        <tr>
+            <br>
+            <td colspan = "1" align = "center">
+                <font Face="DS-Silom" size = "6" color = "#233c62" > Facebook helps you connect and share with </font> 
+            </td>
+        <tr>
+
+        <tr>
+            <br>
+            <td colspan = "2" align = "left" >
+                <font Face="DS-Silom" size = "6" color = "#233c62" > &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; the people in your life. </font> 
+            </td>
+        <tr>
+
+        <tr>
+            <td> <img src = "fp.jpg" border = "0" width = "550" height = "300" align = "center">
+            </td> 
+        <tr>
+
+    <table border = "0" align = "right" width = "500">
+        <tr>
+            <td colspan = "1" align = "center">
+                <font Face = "DS-Silom" size = "10" color = "#222a29" > Create an account </font> 
+            </td>
+        <tr>
+    
+        <tr>
+            <td colspan = "1" align = "left">
+                <font Face = "DS-Silom" size = "6" color = "#222a29" > It's free and alway will be. </font> 
+            </td>
+        <tr>
+        <br>
+        <tr>
+            <td align = "left"> <input type = "text" name = "first" size = "20" value = "" 
+            align = "left"> <input type = "text" name = "sur" size = "20" value = "" /> </td>
+        <tr>
+        
+        <tr>
+            <td align = "left"> <input type = "text" name = "email" size = "60" value = "" /> </td>
+        <tr>
+
+        <tr>
+        <td align = "left"> <input type = "text" name = "pass" size = "60" value = "" /> </td>
+        <tr>
+    
+        <tr>
+            <td colspan = "1" align = "left">
+                <font Face = "DS-Silom" size = "6" color = "#222a29" > Birthday </font> 
+            </td>
+        <tr>
+
+        <td width = 90% colspan = "1" align = "left"> 
+
+        <form>
+            <select>
+                <option> 1
+                <option> 2
+                <option> 3
+                <option> 4
+                <option> 5
+                <option> 6       
+            </select>
+        
+            <select>
+                <option> Jan
+                <option> Feb
+                <option> Mar
+                <option> Apir
+                <option> May
+                <option> Jun     
+            </select>
+     
+            <select>
+                <option> 2021
+                <option> 2020
+                <option> 2019
+                <option> 2018
+                <option> 2017
+                <option>     
+            </select>
+        </form>
+    
+        <input type = "radio" name = "sex" value = "1" /> Female 
+        <input type = "radio" name = "sex" value = "2" /> Male
+    </form>
+       
+    <tr> 
+        <td align = "left"> By cheking Sign Up you agree to our Teams. Data Policy and
+            <br>
+            Cookie Policy You may receive SMS notifications from us and
+            <br>
+            can opt out at ant time.
+        </td>
+
+        <tr>
+            <td colspan = "3" align = "center">
+                <input type = "submit" value = "Sign Up" />
+            <td>
+        <tr>
+
+        
+    </table>    
+
+    <?php
+    
+    ?>
+   
+</body>
+</head>
+</html>
