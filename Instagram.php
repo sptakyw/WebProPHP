e1f1a4fd85bde8c43cbd962b0a487eb7f121958b
sptakyw <sptakyw@gmail.com>
2021-01-24 19:59:59 +0700

    create Instagram.php

---
+++ Instagram.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+    <title> Login • Instagram </title>
+</head>
+
+<body bgcolor = "#fafafa">
+<br> <br> <br> <br> <br>
+<table width = 25%  height = "250" style = "border:3px solid #f2f2f2;" cellspacing = "8" bgcolor = "#ffffff" align = "center" cellpadding = "2">
+    <br> <br>
+
+    <tr>
+            <td> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <img src = "Inlogo.png" border = "0" width = "250" height = "70" align = "center">
+            </td> 
+        <tr>
+
+        <br>
+    <tr>
+           <td align = "center"> &nbsp; &nbsp; &nbsp;<input type = "text" name = "user" size = "35" value = ""> </td>
+    <tr>
+
+    <tr>
+           <td align = "center"> &nbsp; &nbsp; &nbsp;<input type = "password" name = "password" size = "35" value = ""> </td>
+    <tr>
+
+    <tr>
+        <td colspan = "3" align = "center"> &nbsp; &nbsp;<input type = "submit" value = "&nbsp; Log in &nbsp;" /><td>
+    <tr>
+
+    <tr>
+    <td> <font Face = "Arundina Sans" size = "5" color = "#cecece" align = "center" > <b> &nbsp; &nbsp; &nbsp; ――――― OR ――――― </b> </font> </td> 
+    </tr>
+
+    <tr>
+        <td>  <font Face = "Cordia New" size = "5" color = "#385185" align = "center"> 
+            <b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Log in with Facebook </b> </td>
+    </tr>
+
+    <tr>
+    <td>  <font Face = "Cordia New" size = "4" color = "#878787" align = "center"> 
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            &nbsp; Forgot password? </td>
+    </tr>
+
+    <table width = 25% style = "border:3px solid #f2f2f2;" cellspacing = "8" bgcolor = "#ffffff" align = "center" cellpadding = "2">
+    <tr>
+        <td> <font Face = "Cordia New" size = "5" color = "#2a2a2a" align = "center" > 
+            <b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Don't have an account? <font color = "#23a3f7"> Sign up </b> </font> </td> 
+    <tr>
+
+</table>
+        <br>
+         
+
+</body>
+</html>
