8e5c843ba1ae8696d9e2034ecc72d4c5eda52d5d
sptakyw <sptakyw@gmail.com>
2021-01-26 23:56:37 +0700

    create minimal.php

---
+++ minimal.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <title> minimal </title>
+</head>
+<body bgcolor = "#F5DEB3">
+
+    <table width = 100% style = "border:15px solid #FFF8DC;" cellspacing = "5" bgcolor = "#FFF8DC" cellpadding = "2">
+        <tr>
+            <td> <font Face = "DS-Silom" size = "7" color = "#F5DEB3" > ꕥ⌒
+                 <font Face = "DS-Silom" size = "6" color = "#F5DEB3" > <b> 안 녕 </b>
+                 <font Face = "DS-Silom" size = "8" color = "#F5DEB3" > Cafe' ⌒☄ </font> 
+            </td> 
+
+    <table border = "0" align = "center" width = "400">
+        <tr>
+            <br>
+            <td colspan = "1" align = "center">
+                <font Face="DS-Silom" size = "10" color = "#FFFAF0" > welcome to the cafe' </font> 
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan = "1" align = "center">
+            <br>
+                <font Face="DS-Silom" size = "5" color = "#FFFAF0" > <b> ⊱ 어서 오세요 ⊰ </b> </font> 
+            </td>
+        </tr>
+
+    <!-- Login -->
+        <table width = 20% align = "center" style = "border:15px solid #f4d7a1;" cellspacing = "5" bgcolor = "#f4d7a1" cellpadding = "4">
+        <tr>
+        <br>
+            <td> <font Face = "DS-Silom" size = "6" align = "center" color = "#FFFAF0"  > &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;✦✿ Login ☁✦ </font> </td>
+        </tr>
+
+        <tr>
+            <td> <font Face = "DS-Silom" size = "6" align = "center" color = "#FFFAF0"  > ⊹ Username ☂ </font> </td>
+        </tr> 
+
+        <tr>
+            <td align = "center" ><input type = "text" name = "first" style="border: 3px #edd09c solid" size = "25" value = ""> </td>
+        </tr>
+
+        <tr>
+            <td> <font Face = "DS-Silom" size = "6" align = "center" color = "#FFFAF0"  > ⊹ Password ☀ </font> </td>
+        </tr>
+
+        <tr>
+            <td align = "center"> <input type = "password" name = "Password" style="border: 3px #edd09c solid" size = "25" value = ""> </td>
+        </tr>
+        
+
+</body>
+</html>
