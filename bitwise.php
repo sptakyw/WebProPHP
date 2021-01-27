05ab78ab7dd6bfbd9a4eb919a81c8661ce55a4fc
sptakyw <sptakyw@gmail.com>
2021-01-27 10:11:47 +0700

    bitwise.php

---
+++ bitwise.php
@@ -0,0 +1,10 @@
+<?php
+$a = 8;
+$b = 3;
+echo "a & b = ", $a & $b, "\n";
+echo "a | b = ", $a | $b, "\n";
+echo "a ^ b = ", $a ^ $b, "\n";
+echo "~a = ", ~$a, "\n";
+echo "a << 1 = ", $a << 1, "\n";
+echo "a >> 1 = ", $a >> 1, "\n";
+?>
