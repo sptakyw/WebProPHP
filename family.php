937860bcde13a1590cee8f152f8cbbb2dfe73a41
sptakyw <sptakyw@gmail.com>
2021-02-01 22:10:03 +0700

    create family.php

---
+++ family.php
@@ -0,0 +1,11 @@
+<?php
+function familyName($fname) {
+  echo "$fname calnes.<br>";
+}
+
+familyName("Jody");
+familyName("Clint");
+familyName("John");
+familyName("Zoey");
+familyName("Bill");
+?>
