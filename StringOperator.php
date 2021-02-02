dc7969135fe0240b73a02bcccf9a023df9058741
sptakyw <sptakyw@gmail.com>
2021-02-02 23:44:43 +0700

    create StringOperator.php

---
+++ StringOperator.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title> String Operator </title>
+<body>
+    <?php
+        $str = "My"."SQL" ;
+        echo $str."<br>" ;
+        echo "Hypertext"."."Preprocessor"."<br> ;
+        $f_name = "John" ;
+        $l_name = "Smith" ;
+        echo "My name is ".$f_name . "" . l_name . "<br>" ;
+        echo 1 .2 .3 . "<br>" ;
+    ?>
+</body>
+</html>
