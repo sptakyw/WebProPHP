f018a9f3d451fb1097c00090963df2ce50edff97
sptakyw <52157614+sptakyw@users.noreply.github.com>
2021-03-17 12:28:30 +0700

    Add files via upload

---
+++ TestDB/FormCreateDB.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="CreateDB-Form.php" method="get">
+    Name Database : <input type="text" name="nameDB" required><br><br>
+    Name Table : <input type="text" name="nameTB" required><br><br>
+    ชื่อ : <input type="text" name="name" require> นามสกุล <input type="text" name="last" required><br><br>
+    Email : <input type="email" name="email" required><br><br>
+    <input type="submit" name="submit" value ="Submit">
+</form>
+</body>
+</html>
